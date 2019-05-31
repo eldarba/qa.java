e90d267a2a43b89a1b80a5a32cb66cffd79f36d7
you@example.com <you@example.com>
2019-05-31 12:56:05 +0300

    aaa

--- java.lesson.1/src/b/DataTypes.java
+++ java.lesson.1/src/b/DataTypes.java
@@ -3,7 +3,41 @@ package b;
 public class DataTypes {
 
 	public static void main(String[] args) {
+
+		// integers
 		byte n1 = 9;
+		short n2 = 19;
+		int n3 = 95;
+		long n4 = 999999999999L;
 		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		// decimals
+		float f1 = 4.5F;
+		double f2 = 89.32658;
+		System.out.println(f1);
+		System.out.println(f2);
+
+		// use F, D, L - to change default types of literal values
+		// logical
+		boolean b1 = true;
+		boolean b2 = false;
+		boolean b3 = 10 > 100;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+
+		// characters
+		char c1 = 'A';
+		char c2 = 'à';
+		char c3 = '.';
+		char c4 = '-';
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+
 	}
 }
